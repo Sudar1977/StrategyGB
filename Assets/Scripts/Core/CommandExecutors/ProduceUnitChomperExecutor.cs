296623a1710bdc0b6cbc03ad84ac53f6d6156ff0
Sudar1977 <Sudar1977@gmail.com>
2021-06-08 11:20:55 +0300

    РАБОЧИЙ проект, рефакторинг SecondBuilding, добавление ProduceUnitChomperExecutor

---
+++ Assets/Scripts/Core/CommandExecutors/ProduceUnitChomperExecutor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Abstractions;
+
+namespace Core
+{
+    public class ProduceUnitChomperExecutor : CommandExecutorBase<IProduceUnitChomperCommand>
+    {
+        protected override void ExecuteConcreteCommand(IProduceUnitChomperCommand command)
+        {
+            if (command.UnitPrefab == null)
+            {
+                Debug.LogError("No prefab to produce Unit");
+                return;
+            }
+            //Instantiate(command.UnitPrefab, transform.position, Quaternion.identity, transform.parent);
+            var position = new Vector3(Random.Range(-3, 3), 0, Random.Range(-2, 2));// + transform.parent.position;
+
+            Instantiate(command.UnitPrefab, position, Quaternion.identity, transform.parent);
+        }
+    }
+}
--- Assets/Scripts/Core/SecondBuilding.cs
+++ Assets/Scripts/Core/SecondBuilding.cs
@@ -3,27 +3,8 @@ using UnityEngine;
 
 namespace Core
 {
-    public class SecondBuilding : CommandExecutorBase<IProduceUnitChomperCommand>, ISelectableItem
+    public class SecondBuilding : BaseBuilding, ISelectableItem
     {
-        [SerializeField] private Sprite _icon;
-        [SerializeField] private float _health;
-        [SerializeField] private float _maxHealth;
-
-        public Sprite Icon => _icon;
-        public float Health => _health;
-        public float MaxHealth => _maxHealth;
-        protected override void ExecuteConcreteCommand(IProduceUnitChomperCommand command)
-        {
-            if (command.UnitPrefab == null)
-            {
-                Debug.LogError("No prefab to produce Unit");
-                return;
-            }
-            //Instantiate(command.UnitPrefab, transform.position, Quaternion.identity, transform.parent);
-            var position = new Vector3(Random.Range(-3, 3), 0, Random.Range(-2, 2));// + transform.parent.position;
-
-            Instantiate(command.UnitPrefab, position, Quaternion.identity, transform.parent);
-        }
 
     }
 }
